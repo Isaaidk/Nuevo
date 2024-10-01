ee91914afd3563b9c43a00f3fd0eac0b79b317e7
Isaac <Isaacpuga661@gmail.com>
2024-10-01 07:37:55 -0500

    Espacio

---
+++ IsaacPuga-ProyectoMCV/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace IsaacPuga_ProyectoMCV.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
